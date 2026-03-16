bff1fc3af2c2c9df1a649c399da4493477d5150e
Mudit Goyal <muditgoyal31506@gmail.com>
2026-03-16 22:45:43 +0530

    Initial

---
+++ DMA_Bare_Metal/Inc/main.h
@@ -0,0 +1,13 @@
+/*
+ * main.h
+ *
+ *  Created on: 16-Mar-2026
+ *      Author: mudit
+ */
+
+#ifndef MAIN_H_
+#define MAIN_H_
+
+
+
+#endif /* MAIN_H_ */
---
+++ DMA_Bare_Metal/Src/stm32f4xx_it.c
@@ -0,0 +1,14 @@
+/*
+ * stm32f4xx_it.c
+ *
+ *  Created on: 16-Mar-2026
+ *      Author: mudit
+ */
+
+#include <stdint.h>
+#include "stm32f446xx.h"
+
+void EXTI15_10_IRQHandler(void)
+{
+
+}
