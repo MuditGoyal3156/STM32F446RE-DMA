e6deacbad1d41aff1e7fd3e0894243ca3e1465a1
Mudit Goyal <muditgoyal31506@gmail.com>
2026-03-17 22:31:49 +0530

    UART DMA

--- DMA_Bare_Metal/Inc/main.h
+++ DMA_Bare_Metal/Inc/main.h
@@ -8,6 +8,19 @@
 #ifndef MAIN_H_
 #define MAIN_H_
 
+void button_init(void);
+void uart2_init(void);
+void dma1_init(void);
+void send_some_data(void);
+void enable_dma1_stream6(void);
+void dma1_interrupt_configuration(void);
+
+void HT_Complete_callback(void);
+void FT_Complete_callback(void);
+void TE_Complete_callback(void);
+void FE_Complete_callback(void);
+void DME_Complete_callback(void);
+
 
 
 #endif /* MAIN_H_ */
--- DMA_Bare_Metal/Src/stm32f4xx_it.c
+++ DMA_Bare_Metal/Src/stm32f4xx_it.c
@@ -1,14 +1,49 @@
-/*
- * stm32f4xx_it.c
- *
- *  Created on: 16-Mar-2026
- *      Author: mudit
- */
-
 #include <stdint.h>
 #include "stm32f446xx.h"
+#include "main.h"
+
+#define is_it_HT()	DMA1->HISR & (1 << 20)
+#define is_it_FT()	DMA1->HISR & (1 << 21)
+#define is_it_TE()	DMA1->HISR & (1 << 19)
+#define is_it_FE()	DMA1->HISR & (1 << 16)
+#define is_it_DME()	DMA1->HISR & (1 << 18)
 
 void EXTI15_10_IRQHandler(void)
 {
+
+	//USART2_TX sends DMA request to DMA 1 Controller
+	USART2->CR3 |= (1 << 7);
+	if(EXTI->PR & ( 1 << 13))
+	{
+		EXTI->PR |= ( 1 << 13 ); //Clearing the pending bit
+	}
+}
+
+//IRQ handler for DMA1 Stream6 global interrupt
+void DMA1_Stream6_IRQHandler(void)
+{
+	if(is_it_HT())
+	{
+		DMA1->HIFCR |= (1<<20);
+		HT_Complete_callback();
+	}else if(is_it_FT())
+	{
+		DMA1->HIFCR |= (1<<21);
+		FT_Complete_callback();
+	}else if(is_it_TE())
+	{
+		DMA1->HIFCR |= (1<<19);
+		TE_Complete_callback();
+	}else if(is_it_FE())
+	{
+		DMA1->HIFCR |= (1<<16);
+		FE_Complete_callback();
+	}else if(is_it_DME())
+	{
+		DMA1->HIFCR |= (1<<18);
+		DME_Complete_callback();
+	}else{
+
+	}
 
 }
